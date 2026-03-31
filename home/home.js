75440878b43b57d0dca066c0ec26655f06f17491
shreyapnair993 <146433249+shreyapnair993@users.noreply.github.com>
2026-03-31 23:55:14 +0530

    Update home.js

--- home/home.js
+++ home/home.js
@@ -1,10 +1,13 @@
-// home.js
-
-// Logout - asks for confirmation then redirects (US002 - req 5)
+// LOGOUT
 function handleLogout() {
-  var confirmed = confirm("Are you sure you want to logout?");
-  if (confirmed) {
-    localStorage.clear();
-    window.location.href = "../login/login.html";
-  }
+  document.getElementById("confirm_m").style.display = "flex";
+}
+
+function cancel_lg() {
+  document.getElementById("confirm_m").style.display = "none";
+}
+
+function c_logout() {
+  // Redirect to login page
+  window.location.href = "login.html";
 }
